4213e67192552135a50c02509d683d3ad5f72ad5
Karen Mandujano <mankaren1710@gmail.com>
2025-10-13 13:56:36 -0600

    Inicio del repocitrorio y configuracion del mismo

---
+++ Unidad2_Series_De_Fourier/Hola.py
@@ -0,0 +1,24 @@
+#ITESS 
+#Tics 501
+#Analisis de Señales y Sistemas de Comunicacion
+#Diana Karen Mandujano Magaña
+#Programa para comenzar a entender anaconda con Python
+#09 Octubre 2025
+
+print ("Hola Mundo");
+
+#Usar python como calculadora
+#SUMA
+print("SUMA 2 + 2 = ", 2+2)
+#RESTA
+print("RESTA 5 - 2 = ", 5-2)
+#MULTIPLICACION
+print("MULTIPLICACION 5 * 24 = ", 5*24)
+#MULTIPLICACION ** EXPONENTE
+print("EXPONENTE 5 ** 2 = ", 5**2)
+#DIVICION /
+print("DIVISION 5/2 = ", 5/2)
+#DIVISION //
+print("DIVICION // 15//2 = ", 15//2)
+#MODULO %
+print("MODULO 5% 9= ", 5%9)
