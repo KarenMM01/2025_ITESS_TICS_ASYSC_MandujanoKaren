79f410e7793236436b21f0f38b5d196c6d493b6b
Karen Mandujano <mankaren1710@gmail.com>
2025-10-13 18:36:16 -0600

    Tarea de introduccion al lenguaje Python

---
+++ Unidad2_Series_De_Fourier/Cadena de Texto.py
@@ -0,0 +1,32 @@
+#Cadena de Texto 
+'huevos y pan'
+'"Si," le dijo'
+"\"Si,\" le dijo"
+ print("Isn\'t")
+ print(r'c:\algun\nombre')
+ print("""\
+... Uso: algo [OPTIONS]
+... -h                Muestra el mensaje de uso
+... -H nombrehost     Muestra el Host al cual conectarse
+... """)
+3 * 'un' + 'ium'
+'Py' 'thon'
+
+ prefix = 'Py'
+prefix + 'thon'
+ texto = ('Pone muchas cadenas dentro de parentesis' 'para que ellas sean unidas')
+texto
+
+palabra = 'Python'
+palabra [0]
+palabra [5]
+palabra [-1]
+palabra [-2]
+palabra [-6]
+palabra [0:2]
+palabra [2:5]
+palabra[:2] + palabra [2:]
+palabra[:4] + palabra [4:]
+
+s='fgdjdlfbfhyrdlfdnfh'
+len(s)
---
+++ Unidad2_Series_De_Fourier/Uso de la calculadora.py
@@ -0,0 +1,19 @@
+#Uso de la calculadora
+ 2 + 2
+ 50 - 5^6
+ (50 - 5*6)/4
+ 8 / 5
+ 17 / 3
+17 // 3
+ 17 % 3
+ 5 ** 2
+2 ** 7
+
+ ancho = 20
+largo = 5 * 9
+ancho * largo
+
+impuesto = 12.5 / 100
+precio = 100.50
+precio * impuesto
+ round(_,2)
